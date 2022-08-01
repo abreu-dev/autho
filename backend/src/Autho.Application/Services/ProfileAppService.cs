2fe7c15e92e8b7c8efad6d3589729d697b2303c7
Gabriel Abreu <gabriel.roberto.abreu@gmail.com>
2022-07-31 21:54:24 -0300

    some improvements

--- backend/src/Autho.Application/Services/ProfileAppService.cs
+++ backend/src/Autho.Application/Services/ProfileAppService.cs
@@ -21,12 +21,6 @@ namespace Autho.Principal
                 return new Result(ResultType.Failure, new ResultError("AlreadyUsed", "Already Used", string.Format(AuthoResource.NameAlreadyInUse, creationDto.Name)));
             }
 
-            var resultPermission = ValidatePermissions(creationDto.Permissions);
-            if (resultPermission.Type == ResultType.Failure)
-            {
-                return resultPermission;
-            }
-
             var permissions = creationDto.Permissions.Select(x => new PermissionDomain(x.Id)).ToList();
             var profile = new ProfileDomain(creationDto.Name, permissions);
 
@@ -41,12 +35,6 @@ namespace Autho.Principal
             if (_profileRepository.ExistsName(id, creationDto.Name))
             {
                 return new Result(ResultType.Failure, new ResultError("AlreadyUsed", "Already Used", string.Format(AuthoResource.NameAlreadyInUse, creationDto.Name)));
-            }
-
-            var resultPermission = ValidatePermissions(creationDto.Permissions);
-            if (resultPermission.Type == ResultType.Failure)
-            {
-                return resultPermission;
             }
 
             var profile = _profileRepository.GetWithPermissions(id);
@@ -71,40 +59,6 @@ namespace Autho.Principal
         {
             _profileRepository.Delete(id);
             _profileRepository.UnitOfWork.Complete();
-
-            return new Result(ResultType.Success);
-        }
-
-        private IResult ValidatePermissions(IEnumerable<ProfilePermissionCreationDto> permissions)
-        {
-            if (permissions != null)
-            {
-                var duplicatedPermissions = permissions.GroupBy(x => x.Id).Where(x => x.Count() > 1).ToList();
-                if (duplicatedPermissions.Any())
-                {
-                    var result = new Result(ResultType.Failure);
-                    duplicatedPermissions.ForEach(duplicatedPermission =>
-                    {
-                        result.Errors.Add(new ResultError("Duplicated", "Duplicated", AuthoResource.DuplicatedPermission));
-                    });
-                    return result;
-                }
-
-                var failedPermissions = new List<Guid>();
-                foreach (var _ in permissions.Where(permission => !_permissionRepository.Exists(permission.Id)))
-                {
-                    failedPermissions.Add(_.Id);
-                }
-                if (failedPermissions.Any())
-                {
-                    var result = new Result(ResultType.Failure);
-                    failedPermissions.ForEach(failedPermission =>
-                    {
-                        result.Errors.Add(new ResultError("NotFound", "Not Found", AuthoResource.PermissionNotFound));
-                    });
-                    return result;
-                }
-            }
 
             return new Result(ResultType.Success);
         }
--- backend/src/Autho.Infra.CrossCutting.Globalization/Language.cs
+++ backend/src/Autho.Infra.CrossCutting.Globalization/Language.cs
@@ -1,17 +1,10 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Autho.Infra.CrossCutting.Globalization
 {
     public enum Language
     {
         [Display(Description = "en")]
-        EN = 0,
-
-        [Display(Description = "es")]
-        ES = 1,
-
-        [Display(Description = "pt-BR")]
-        PT_BR = 2
+        EN = 0
     }
 }
